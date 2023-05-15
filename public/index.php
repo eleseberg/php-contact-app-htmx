01e1a464b3cea02b90dd65ff10828cdce84400d5
Ernie Leseberg <ernie@leseberg.org>
2023-05-15 13:07:36 +1000

    Twig installed with a base layout template.

--- public/index.php
+++ public/index.php
@@ -2,16 +2,27 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+// Create Twig
+$twig = Twig::create(__DIR__ . '/../app/templates');
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+// Routes
 $app->redirect('/', '/contacts', 301);
 
 $app->get('/contacts', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello contacts!");
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'contacts.html.twig', [
+        'name' => $args['name']
+    ]);
 });
 
 $app->run();
