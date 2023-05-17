ed7eb0ac3c2e28eba96e46a24d31adbe54a5e2de
Ernie Leseberg <ernie@leseberg.org>
2023-05-17 14:14:07 +1000

    Implement the show, edit and delete of contacts. Starter data file out of public

--- app/model/Contact.php
+++ app/model/Contact.php
@@ -4,48 +4,191 @@ namespace App\model;
 
 class Contact
 {
-    public string $filePath;
-
     /**
-     * mock contacts database
+     * Mock contacts database
      *
      * @var array
      */
-    public array $db;
+    private static array $db = [];
+    private array $errors;
 
     /**
      * Constructor
      *
-     * @throws \Exception
+     * @param int|null $id
+     * @param string|null $first
+     * @param string|null $last
+     * @param string|null $phone
+     * @param string|null $email
      */
-    public function __construct()
+    public function __construct(
+        public ?int $id,
+        public ?string $first,
+        public ?string $last,
+        public ?string $phone,
+        public ?string $email
+    ) {
+        $this->errors = [];
+    }
+
+    public function __toString()
     {
-        $this->filePath = realpath(__DIR__ . "/../../public/data.json");
-        $this->loadDb();
+        return json_encode($this, JSON_UNESCAPED_UNICODE);
     }
 
     /**
-     * Return all contact records
-     *
-     * @return array[]
+     * @return int|null
      */
-    public function all(): array
+    public function id(): ?int
     {
-        return $this->db;
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function first(): ?string
+    {
+        return $this->first;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function last(): ?string
+    {
+        return $this->last;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function phone(): ?string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function email(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return array
+     */
+    public function errors(): array
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @param string $first
+     * @param string $last
+     * @param string $phone
+     * @param string $email
+     * @return void
+     */
+    public function update(string $first, string $last, string $phone, string $email): void
+    {
+        $this->first = $first;
+        $this->last = $last;
+        $this->phone = $phone;
+        $this->email = $email;
+    }
+
+    /**
+     * @return bool
+     */
+    public function validate(): bool
+    {
+        if (!$this->email) {
+            $this->errors['email'] = "Email Required";
+        }
+        $existingContact = null;
+        foreach (self::$db as $contact) {
+            if ($contact->id !== $this->id && $contact->email === $this->email) {
+                $existingContact = $contact;
+                break;
+            }
+        }
+        if ($existingContact) {
+            $this->errors['email'] = "Email Must Be Unique";
+        }
+        return count($this->errors) === 0;
+    }
+
+    /**
+     * @return bool
+     */
+    public function save(): bool
+    {
+        if (!$this->validate()) {
+            return false;
+        }
+        if ($this->id === null) {
+            if (empty(self::$db)) {
+                $maxId = 1;
+            } else {
+                $maxId = max(array_column(self::$db, 'id')) + 1;
+            }
+            $this->id = $maxId;
+            self::$db[$this->id] = $this;
+        }
+        self::saveDB();
+        return true;
+    }
+
+    /**
+     * @return void
+     */
+    public function delete(): void
+    {
+        unset(self::$db[$this->id]);
+        self::saveDB();
+    }
+
+    /**
+     * @return int
+     */
+    public static function count(): int
+    {
+        sleep(2);
+        return count(self::$db);
+    }
+
+    /**
+     * Return all contact records for a page
+     *
+     * @param int $page
+     * @return array
+     */
+    public static function all(int $page = 1): array
+    {
+        $start = ($page - 1) * PAGE_SIZE;
+        $end = $start + PAGE_SIZE;
+        return array_slice(array_values(self::$db), $start, $end - $start);
     }
 
     /**
      * Search contact records for given text
      *
-     * @param string $text
+     * @param $text
      * @return array
      */
-    public function search(string $text): array
+    public static function search($text): array
     {
         $result = [];
-        foreach ($this->db as $record) {
-            if (in_array($text, $record)) {
-                $result[] = $record;
+        foreach (self::$db as $contact) {
+            if (
+                str_contains($contact->first, $text) ||
+                str_contains($contact->last, $text) ||
+                str_contains($contact->email, $text) ||
+                str_contains($contact->phone, $text)
+            ) {
+                $result[] = $contact;
             }
         }
         return $result;
@@ -57,21 +200,50 @@ class Contact
      * @return void
      * @throws \Exception
      */
-    private function loadDb(): void
+    public static function loadDB(): void
     {
-        // Read JSON data from file
-        $jsonData = file_get_contents($this->filePath);
+        $filePath = realpath(__DIR__ . "/../data/data.json");
 
-        // Convert JSON to array
-        $data = json_decode($jsonData, true);
+        try {
+            $contactsFile = file_get_contents($filePath);
+        } catch (\Exception $e) {
+            throw $e;
+            //throw new \Exception('Error accessing JSON data at ' . $filePath);
+        }
 
-        // Check if JSON decoding was successful
-        if ($data === null) {
-            throw new \Exception('Error decoding JSON data from ' . $this->filePath);
-        } else {
-            // Use the $data array as needed
-            $this->db = $data;
+        $contacts = json_decode($contactsFile, true);
+        self::$db = [];
+        foreach ($contacts as $c) {
+            self::$db[$c['id']] = new Contact($c['id'], $c['first'], $c['last'], $c['phone'], $c['email']);
         }
     }
 
+    /**
+     * @return void
+     */
+    public static function saveDB(): void
+    {
+        $filePath = realpath(__DIR__ . "/../data/data.json");
+
+        $outArr = [];
+        foreach (self::$db as $contact) {
+            $outArr[] = $contact;
+        }
+
+        file_put_contents($filePath, json_encode($outArr, JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * @param $id
+     * @return Contact
+     */
+    public static function find($id): Contact
+    {
+        $id = intval($id);
+        $contact = self::$db[$id] ?? null;
+        if ($contact !== null) {
+            $contact->errors = [];
+        }
+        return $contact;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\model\Contact;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -6,6 +8,8 @@ use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+const PAGE_SIZE = 100;
 
 $app = AppFactory::create();
 
@@ -16,19 +20,21 @@ $twig = Twig::create(__DIR__ . '/../app/templates');
 $app->add(TwigMiddleware::create($app, $twig));
 
 // Routes
+
+// Redirect home to all contacts
 $app->redirect('/', '/contacts', 301);
 
+// Show all contacts
 $app->get('/contacts', function (Request $request, Response $response, $args) {
-
-    $contacts = new App\model\Contact();
+    Contact::loadDB();
     $searchTerm = '';
     $queryTerms = $request->getQueryParams();
 
     if (array_key_exists('q', $queryTerms)) {
         $searchTerm = $queryTerms['q'];
-        $contactsSet = $contacts->search($searchTerm);
+        $contactsSet = Contact::search($searchTerm);
     } else {
-        $contactsSet = $contacts->all();
+        $contactsSet = Contact::all();
     }
 
     $view = Twig::fromRequest($request);
@@ -36,6 +42,90 @@ $app->get('/contacts', function (Request $request, Response $response, $args) {
         'contacts' => $contactsSet,
         'searchTerm' => $searchTerm
     ]);
+});
+
+// Show a Contact
+$app->get('/contacts/{id:[0-9]+}', function (Request $request, Response $response, $args) {
+    Contact::loadDB();
+    $contact = Contact::find($args['id']);
+
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'show.html.twig', [
+        'contact' => $contact
+    ]);
+});
+
+// Show new form
+$app->get('/contacts/new', function (Request $request, Response $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'new.html.twig', []
+    );
+});
+
+// Add a contact
+$app->post('/contacts/new', function (Request $request, Response $response, $args) {
+    Contact::loadDB();
+    $parsedBody = $request->getParsedBody();
+
+    $c = new Contact(null, $parsedBody['first_name'], $parsedBody['last_name'], $parsedBody['phone'], $parsedBody['email']);
+
+    if ($c->save()) {
+        // flash("Created New Contact!")
+        return $response
+            ->withHeader('Location', '/contacts')
+            ->withStatus(302);
+    } else {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'new.html.twig', [
+                                          'contact' => $c
+                                      ]
+        );
+    }
+});
+
+// Form to edit a contact
+$app->get('/contacts/{id:[0-9]+}/edit', function (Request $request, Response $response, $args) {
+    Contact::loadDB();
+    $contact = Contact::find($args['id']);
+
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'edit.html.twig', [
+        'contact' => $contact
+    ]);
+});
+
+// Edit a Contact
+$app->post('/contacts/{id:[0-9]+}/edit', function (Request $request, Response $response, $args) {
+    Contact::loadDB();
+    $c = Contact::find($args['id']);
+
+    $parsedBody = $request->getParsedBody();
+    $c->update($parsedBody['first_name'], $parsedBody['last_name'], $parsedBody['phone'], $parsedBody['email']);
+
+    $view = Twig::fromRequest($request);
+    if ($c->save()) {
+        //flash("Updated Contact!")
+        return $response
+            ->withHeader('Location', '/contacts/' . $c->id() )
+            ->withStatus(302);
+    } else {
+        return $view->render($response, 'edit.html.twig', [
+            'contact' => $c
+        ]);
+    }
+});
+
+// Delete a contact
+$app->post('/contacts/{id:[0-9]+}/delete', function (Request $request, Response $response, $args) {
+    Contact::loadDB();
+    $c = Contact::find($args['id']);
+
+    $c->delete();
+    //flash("Deleted Contact!")
+
+    return $response
+        ->withHeader('Location', '/contacts')
+        ->withStatus(302);
 });
 
 $app->run();
